e0a174e9a1b8c82ab00d523b111881cd57553135
Motoki Nagano <motoki.n.310@gmail.com>
2017-06-14 10:06:12 +0900

    初期動作確認のためのコードを削除

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
-ini_set("display_errors", On);
-error_reporting(E_ALL);
+    ini_set("display_errors", On);
+    error_reporting(E_ALL);
 ?>
-
-<html>
-    <head>
-        <title>Test</title>
-    </head>
-    <body>
-        <p><?php echo 'Hello World.' ?></p>
-    </body>
-</html>
 
 <?php
     define('_ROOT_DIR', __DIR__.'/');
