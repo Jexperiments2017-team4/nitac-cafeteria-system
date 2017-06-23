5eb1d1329373231e92b87018299098a6a955a505
spade630 <spade6301@gmail.com>
2017-06-23 11:59:47 +0900

    関数名修正

--- apps/models/Product.php
+++ apps/models/Product.php
@@ -1,6 +1,6 @@
 <?php
 class Product extends BaseModel {
-  public function get_data($view){
+  public function set_data($view){
     $result = pg_query('SELECT * FROM products');
     if(!$result){
       die('クエリ取得に失敗しました。'.pg_last_error());
